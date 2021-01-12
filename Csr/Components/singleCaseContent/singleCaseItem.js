c3199d739a4e526b4a8e0eba5269ce89cbe0966a
AndroAppa <pangana13@gmail.com>
2021-01-12 13:41:27 +0400

    12 jan

---
+++ Csr/Components/singleCaseContent/singleCaseItem.js
@@ -0,0 +1,32 @@
+export const SingleCaseItem = ({ item, link, getMetaLink, type, docId }) => {
+  const handleMouseEnter = () => {
+    getMetaLink(link);
+  };
+
+  return (
+    <>
+      {item ? (
+        <div
+          className="single_case_item"
+          link={item.link}
+          onMouseEnter={handleMouseEnter}
+        >
+          {type === "image" ? (
+            <img className="single_case-image" src={item.link} />
+          ) : type === "video" ? (
+            <video className="single_case-video" src={item.link} />
+          ) : null}
+
+          <div
+            className="fb-share-button"
+            onMouseEnter={() => {
+              console.log("Mouse down");
+            }}
+            data-size="large"
+            data-href={`https://powerful-thicket-90466.herokuapp.com/about?docId=${docId}`}
+          ></div>
+        </div>
+      ) : null}
+    </>
+  );
+};
--- pages/about/index.js
+++ pages/about/index.js
@@ -4,9 +4,11 @@ import Router, { useRouter } from "next/router";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { _url } from "../../Csr/_urls";
+import { SingleCaseItem } from "../../Csr/Components/singleCaseContent/singleCaseItem";
 
 export default function AboutPage({ content }) {
   const [mounted, setMounted] = useState(false);
+  const [metaLink, setMetaLink] = useState(content.data.Url[0].link);
   const router = useRouter();
 
   useEffect(() => {
@@ -15,11 +17,18 @@ export default function AboutPage({ content }) {
       loadFbSdk();
     }
 
-    console.log(router.query.docId, " ############ Router");
+    // console.log(router.query.docId, " ############ Router");
   });
 
   const clickHandler = () => {
     Router.push("/");
+  };
+
+  const getMetaLink = (link) => {
+    if (link !== metaLink) {
+      setMetaLink(link);
+    }
+    console.log(link, " <- link");
   };
 
   const loadFbSdk = () => {
@@ -49,7 +58,7 @@ export default function AboutPage({ content }) {
   const { address, type, description, Url, upload_date } = content.data;
   const docId = router.query.docId;
 
-  console.log(docId, " ####### docId form about");
+  // console.log(docId, " ####### docId form about");
 
   return (
     <>
@@ -62,33 +71,32 @@ export default function AboutPage({ content }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Your Website Title" />
         <meta property="og:description" content="Your description" />
-        <meta
-          property="og:image"
-          content="https://storage.googleapis.com/deligation-40179.appspot.com/IMG_20201013_195931.jpg"
-        />
+        <meta property="og:image" content={metaLink} />
       </Head>
-      <div className="single_case_container">
+      <div
+        className="single_case_container"
+        onMouseEnter={() => {
+          console.log("Mouse down on container");
+        }}
+      >
         <div className="single_case-type">{type.toUpperCase()}</div>
         <div className="single_case-address">{address}</div>
         <div className="single_case-upload_date">{upload_date}</div>
         <div className="single_case_content">
           {content.data.Url.map((item, index) => {
-            return item.type === "image" ? (
-              <img key={index} className="single_case-image" src={item.link} />
-            ) : item.type === "video" ? (
-              <video
+            return (
+              <SingleCaseItem
+                getMetaLink={getMetaLink}
                 key={index}
-                className="single_case-video"
-                src={item.link}
+                link={item.link}
+                item={item}
+                type={item.type}
+                docId={docId}
               />
-            ) : null;
+            );
           })}
         </div>
         <button onClick={clickHandler}> to Home</button>
-        <div
-          className="fb-share-button"
-          data-href={`https://powerful-thicket-90466.herokuapp.com/about?docId=${docId}`}
-        ></div>
       </div>
     </>
   );
@@ -102,7 +110,7 @@ AboutPage.getInitialProps = async (ctx) => {
 
   const data = await response.json();
 
-  console.log(data, "###### data from initialProps");
+  // console.log(data, "###### data from initialProps");
 
   return {
     content: data,
